0e1682f6effaa1903d7837e0004d2a4e59d28b65
김한철 <gkscjf137@naver.com>
2024-11-15 22:35:38 +0900

    git status

---
+++ src/components/Menu.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const Menu = (props) => {
+  const { foods } = props;
+  const [searchType, setSearchType] = useState("전체");
+  const {setFoods} = props;
+  const {setShowModal} = props;
+
+  function Foodmenu({ menu }) {
+    return <li>{menu} <button onClick={() => handleDelete(menu)}>x</button></li>;
+  }
+
+  const handlSearch = (type) => {
+    console.log(type);
+    setSearchType(type);
+  };
+  const handleDelete = (menu) => {
+    console.log("삭제할 메뉴:", menu);
+  
+      setFoods((prevFoods) =>
+      prevFoods.map((food) => ({
+        ...food,
+        foodMenu: food.foodMenu.filter((item) => item !== menu),
+      }))
+    );
+  };
+  const ModalOpen = (foodType) => {
+    
+    setShowModal(true);
+  }
+
+  const handleDeleteTitle = (foodType) => {
+    console.log("삭제할 메뉴 타입:", foodType);
+
+    setFoods((prevFoods) => 
+    prevFoods.filter((item)=> item.foodType !== foodType), 
+    );
+  }
+
+  return (
+    <div>
+      <div className="menuBtn">
+        <button onClick={() => handlSearch("전체")}>전체</button>
+        {foods.map((type, idx) => (
+          <button key={idx} onClick={() => handlSearch(`${type.foodType}`)}>
+            {type.foodType}
+          </button>
+        ))}
+      </div>
+
+      {foods
+        .filter((food) => searchType === "전체" || food.foodType === searchType)
+        .map((food, idx) => (
+          <div key={idx} className="menuDiv" onClick={() => ModalOpen(food.foodType)}>
+            <h3>{food.foodType} <button onClick={() => handleDeleteTitle(food.foodType)}>x</button></h3>
+            <ul>
+              {food.foodMenu.map((menu, idx) => (
+                // <li key={menu + idx}>{menu}</li>
+                <div>
+                  <Foodmenu key={menu + idx} menu={menu} /> 
+                </div>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Menu;
